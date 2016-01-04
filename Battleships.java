2f258a57796df3cac6f753e80cd87774631e9fb1
Elise Ratcliffe <elise@eliseratcliffe.co.uk>
2016-01-04 21:28:05 +0000

    initial commit

---
+++ Battleships.java
@@ -0,0 +1,14 @@
+public class Battleships
+{
+
+    private static UserInterface ui = new TextUserInterface();
+
+    public static void main(String args[])
+    {
+        ui.displayASCII();
+
+    }
+
+
+
+}
---
+++ TextUserInterface.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.Writer;
+import java.io.PrintWriter;
+import java.io.OutputStreamWriter;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class TextUserInterface implements UserInterface
+{
+
+    public void displayASCII()
+    {
+        String fileName = "ASCII.txt";
+
+        String line = null;
+        ArrayList<String> arrayList = new ArrayList<String>();
+        try
+        {
+            FileReader fileReader = new FileReader(fileName);
+            BufferedReader br = new BufferedReader(fileReader);
+            while ((line = br.readLine()) != null)
+            {
+                arrayList.add(line);
+            }
+            br.close();
+        }
+
+        catch (FileNotFoundException ex)
+        {
+            //ui.displayException("a FileNotFoundException");
+            System.exit(0);
+        }
+
+        catch (IOException e)
+        {
+            //ui.displayException("an IOException");
+            System.exit(0);
+        }
+
+        for (int i=0; i<arrayList.size(); i++)
+        {
+            System.out.println(arrayList.get(i));
+        }
+
+    }
+
+}
