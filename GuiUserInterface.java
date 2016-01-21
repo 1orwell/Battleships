9a5d9b4a837e01a63da77f85328e42e18c178bed
Elise Ratcliffe <elise@eliseratcliffe.co.uk>
2016-01-21 20:30:36 +0000

    made board fully, with letters and numbers grid.

--- GuiUserInterface.java
+++ GuiUserInterface.java
@@ -7,13 +7,14 @@ public class GuiUserInterface
     {
         JFrame f = new JFrame();
         f.setSize(800, 800);
-        for (int j=0; j<11; j++)
+        for (int row=0; row<11; row++)
         {
-            for (int i=0; i<11; i++)
+            for (int column=0; column<11; column++)
             {
-                int xIndex = (i*80)+10;
-                int yIndex = (j*80)+10;
-                String[] letters = new String[10];
+                System.out.println("row: " + row+". column: "+column);
+                int xIndex = (column*70)+5;
+                int yIndex = (row*70)+5;
+                String[] letters = new String[11];
                 letters[0] = "A";
                 letters[1] = "B";
                 letters[2] = "C";
@@ -24,23 +25,29 @@ public class GuiUserInterface
                 letters[7] = "H";
                 letters[8] = "I";
                 letters[9] = "J";
-                if (j==0 && i != 0)
+                if (column==0 && row==0)
                 {
-                    System.out.println("j: "+Integer.toString(j) + ". i: "+Integer.toString(i-1));
-                    String letter = letters[i-1];
-                    JButton b = new JButton(letter);
-                    b.setBounds(xIndex, 10, 60, 60); // x axis, y axis, width, height
+                    JButton b = new JButton("");
+                    b.setBounds(5, 5, 50, 50); // x axis, y axis, width, height
                     f.add(b);
                 }
-                if (i==0)
+                if (column==0 && row != 0)
                 {
-                    String jString = Integer.toString(j+1);
+                    String jString = Integer.toString(row);
                     JButton b = new JButton(jString);
-                    b.setBounds(10, yIndex, 60, 60); // x axis, y axis, width, height
+                    b.setBounds(5, yIndex, 50, 50); // x axis, y axis, width, height
+                    f.add(b);
+                }
+                if (row==0 && column != 0)
+                {
+                    System.out.println("row: "+Integer.toString(row) + ". column: "+Integer.toString(column-1));
+                    String letter = letters[column-1];
+                    JButton b = new JButton(letter);
+                    b.setBounds(xIndex, 5, 50, 50); // x axis, y axis, width, height
                     f.add(b);
                 }
                 JButton b = new JButton("-");
-                b.setBounds(xIndex, yIndex, 60, 60); // x axis, y axis, width, height
+                b.setBounds(xIndex, yIndex, 50, 50); // x axis, y axis, width, height
                 f.add(b);
             }
         }
